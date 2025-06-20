f893d38ae76d065e8b8a22a932d4ddcf6c95979c
Randall Wolfe <118763950+raworiginal@users.noreply.github.com>
2025-06-19 18:49:47 -0700

    Update app.js

--- app.js
+++ app.js
@@ -105,9 +105,9 @@ Example: maxOfThree(17, 4, 9) should return 17.
 Complete the exercise in the space below:
 */
 const maxOfThree = (num1, num2, num3) => {
-  if (num1 > num2 && num3) {
+  if (num1 > num2 && num1 > num3) {
     return num1;
-  } else if (num2 > num1 && num3) {
+  } else if (num2 > num1 && num 2 > num3) {
     return num2;
   } else {
     return num3;
